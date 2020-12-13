f90579c9dd186eb74a7cccf8237f0330e2147b5f
dexkode4 <dexkode4@gmail.com>
2020-12-13 22:28:41 +0100

    add footer page

--- src/pages/footer/index.tsx
+++ src/pages/footer/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 import styles from './footer.module.scss';
 
 import { IconFacebook, IconInstagram, IconTwitter } from '../../assets/svg'
@@ -10,6 +11,23 @@ function Footer() {
             <div className={styles.footerLogo}>
                 easycontact
             </div>
+
+            <div className={styles.footerSocial}>
+                <IconFacebook className={styles.footerSocialIcon} />
+                <IconInstagram className={styles.footerSocialIcon} />
+                <IconTwitter className={styles.footerSocialIcon} />
+            </div>
+
+            <nav>
+                <ul>
+                    <li> <Link to="/about">About us</Link></li>
+                    <li> <Link to="/contact">contact</Link> </li>
+                    <li> <Link to="/support">support</Link></li>
+                    <li> <Link to="/privacy">privacy policy</Link> </li>
+                </ul>
+            </nav>
+
+            <p>&copy; Easycontact. All Rights Reserved</p>
         </div>
     )
 }
